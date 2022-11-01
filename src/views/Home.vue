58375ecef430ead1db2c732a5205fb6f9a30a980
nisino25 <nisino25@gmail.com>
2022-11-01 22:20:45 +0900

    changed the firebase databse

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="home">
     <!-- <h1 style="text-align: left; margin-left:25px;">Home</h1> -->
-    <h1 style="display: block; text-align: center;">Home</h1>
+    <h1 style="display: block; text-align: center;" @click="test()">Home</h1>
+    <!-- <span v-if="wholeData">{{wholeData[0]}}</span> -->
 
-    <div class="content-wrapper" v-if="dummyData" style="margin-bottom: 100px; ">
-      <template v-for="(article,i) in dummyData" :key="i">
+    <div class="content-wrapper" v-if="wholeData" style="margin-bottom: 100px; ">
+      <template v-for="(article,i) in wholeData[0]" :key="i">
         <div class="article" >
           <div class="article-left">
             <img :src="article.thmbnail_img_url" alt="">
@@ -13,7 +14,7 @@
           <div class="article-right"  @click="openInNewTab(`${article.atricle_url}`)">
             <strong style=" display:inline-block;">{{article.title}}</strong><br>
 
-            <span style="margin-top: 50px; font-size: 85%">{{article.lead_text}}...</span><br>
+            <span style="margin-top: 50px; font-size: 85%">{{article.lead_text}}</span><br>
 
             <small>{{article.posted_at}}</small>
 
@@ -61,6 +62,7 @@ export default {
     return{
       // db,
       dummyData: undefined,
+      wholeData: undefined,
     }
   },
 
@@ -70,28 +72,96 @@ export default {
     },
 
     async getTheData(){ 
-      var docRef = db.collection('daily_news_20221029');
+      var docRef = db.collection('daily_news');
+      docRef.get().then((doc) => {
+        this.wholeData = []
+        
+
+        // this.dummyData = []
+        // console.table(this.dummyData)
+        doc.forEach(doc => {
+          this.wholeData.push(doc.data().data)
+        });
+
+        console.log(this.wholeData)
+        // for(let i in this.dummyData){
+        //   let data = this.dummyData[i]
+        //   // console.log(i)
+        //   data.lead_text = data.lead_text.substr(0, 50) + "\u2026";
+        // }
+        // console.log(this.dummyData)
+      }).catch((error) => {
+          console.log("Error getting document:", error);
+      }); 
+        
+
+        // ------------------------------------------------
+      // let query
+      // const d = new Date();
+      // query = `${d.getFullYear()}-${d.getMonth-1}-${d.getDate}`
+
+      // function getPreviousDay(date = new Date()) {
+      //   const previous = new Date(date.getTime());
+      //   previous.setDate(date.getDate() - 1);
+
+      //   return previous;
+      // }
+
+      // const theYear = getPreviousDay().getFullYear()
+      // const theMonth =getPreviousDay().getMonth() +1
+      // const thedate =getPreviousDay().getDate()
+      // query =`${theYear}${theMonth}${thedate }`
+
+      // var docRef = db.collection('daily_news').doc(query);
+      // var docRef = db.collection('daily_news')
+      // console.log(query)
+      // var docRef = db.collection('daily_news_20221029')
+      
        
 
-        docRef.get().then((doc) => {
-          this.dummyData = []
-          // console.table(this.dummyData)
+        // docRef.get().then((doc) => {
+          // if (doc.exists) {
+            // console.log(doc.data())
+            // this.dummyData = (doc.data().data)
 
-          doc.forEach(doc => {
-            this.dummyData.push(doc.data())
-            // count++
-            // console.
-            // console.log(doc.id, '=>', doc.data());
-          });
-          for(let i in this.dummyData){
-            let data = this.dummyData[i]
-            // console.log(i)
-            data.lead_text = data.lead_text.substr(0, 50) + "\u2026";
-          }
+            // for(let i in this.dummyData){
+            //   let data = this.dummyData[i]
+            //   // let length = data.title.length + data.lead_text.length
+            //   data.title+= `:${data.title.length}, ${data.lead_text.length}`
+              // if(length > 100){
+              //   data.lead_text = data.lead_text.substr(0, 30) 
+              //   data.lead_text += '!!!'
+              //   // data.lead_text = 'syo'
+              // }
+            // }
+          // }else{
+          //   console.log('doesnot exist')
+          // }
+
+          // this.dummyData = []
+          // // // console.table(this.dummyData)
+
+          // doc.forEach(doc => {
+          //   this.dummyData.push(doc.data())
+          //   // count++
+          //   // console.
+          //   // console.log(doc.id, '=>', doc.data());
+          // });
+          // for(let i in this.dummyData){
+          //   let data = this.dummyData[i]
+          //   let length = data.title.length
+          //   if(length + data.lead_text.length > 125){
+          //     data.lead_text = data.lead_text.substr(0, (180 - 125)) 
+          //     data.lead_text += '...'
+          //     // data.lead_text = 'syo'
+          //   }
+          // //   // console.log(i)
+           
+          // }
           // console.log(this.dummyData)
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        }); 
+        // }).catch((error) => {
+        //     console.log("Error getting document:", error);
+        // }); 
 
         
 
@@ -99,6 +169,28 @@ export default {
         // "abcdef".substr(0, 3) + "\u20/26";
       
       
+    },
+
+    async test(){
+      console.log('hey')
+
+      let data = []
+      let x= 0 
+      let y= 0
+      data.push(this.wholeData[y][1  + x])
+      data.push(this.wholeData[y][2 + x])
+      data.push(this.wholeData[y][3 + x])
+
+      // Add a new document in collection "cities" with ID 'LA'
+      // const res = await db.collection('daily-news').doc('20221029').set(data);
+
+      // this.historyArticles[this.tempIndex-1].content= this.tempContent
+      // this.historyArticles = JSON.stringify(this.historyArticles)
+
+      const ref = db.collection('daily_news')
+      ref.doc(`20221029`).set(data)
+
+      // console.log('updagting')
     },
   },
 
